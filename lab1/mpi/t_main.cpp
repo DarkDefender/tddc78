33b778346012d6486c40b5c8aab83981c3f58df2
Sebastian Parborg <darkdefende@gmail.com>
2016-04-18 11:05:15 +0200

    Lab1 thres now working

--- lab1/mpi/t_main.cpp
+++ lab1/mpi/t_main.cpp
@@ -6,7 +6,7 @@
 
 #define MAX_MEM 1024*1024
 #define MAX_RAD 1000
-#define ROOT_ID 1
+#define ROOT_ID 0
 
 /* NOTE: This structure must not be padded! */
 typedef struct _pixel {
@@ -15,8 +15,15 @@ typedef struct _pixel {
 
 using namespace std;
 
-unsigned sum_chunk( pixel *buf, int buf_size) {
+void check_alloc(void *ptr){
+	if(ptr == NULL){
+		fprintf(stderr, "out of memory\n");
+	}
+}
+
+unsigned sum_chunk( pixel *buf, int char_buf_size) {
 	unsigned sum = 0;
+	int buf_size = char_buf_size/3;
 
 	for( int i = 0; i < buf_size; i++){
 		sum += (unsigned)(buf[i].r + buf[i].g + buf[i].b);
@@ -27,8 +34,9 @@ unsigned sum_chunk( pixel *buf, int buf_size) {
 	return sum;
 }
 
-void thres_chunk( pixel *buf, int buf_size, unsigned tot_sum) {
+void thres_chunk( pixel *buf, int char_buf_size, unsigned tot_sum) {
 	unsigned sum;
+	int buf_size = char_buf_size/3;
 
 	for( int i = 0; i < buf_size; i++){
 		sum = (unsigned)(buf[i].r + buf[i].g + buf[i].b);
@@ -54,6 +62,8 @@ void thres_main(int myid, int p_tot, char *img_path) {
 
 		data = (pixel *)malloc(MAX_PIXELS*sizeof(pixel));
 
+		check_alloc(data);
+
 		read_ppm( img_path, &x_size, &y_size, &max, (char *) data);
 
 		int size = x_size * y_size;
@@ -61,7 +71,11 @@ void thres_main(int myid, int p_tot, char *img_path) {
 		int last_chunk_pad = size - chunk_size * p_tot;
 
 		displs = (int *)malloc(p_tot*sizeof(int));
+		check_alloc(displs);
+
 		scounts = (int *)malloc(p_tot*sizeof(int));
+
+		check_alloc(scounts);
 
 		for( int i = 0; i < p_tot; i++){
 			displs[i] = i*chunk_size*3;
@@ -79,7 +93,10 @@ void thres_main(int myid, int p_tot, char *img_path) {
 
 	MPI_Scatter(scounts, 1, MPI_INT, &recv_count, 1, MPI_INT, ROOT_ID, comm);
 
-	pixel recv_buf[ recv_count ];
+	pixel *recv_buf;
+
+	recv_buf = (pixel *)malloc(recv_count*sizeof(char));
+	check_alloc(recv_buf);
 
 	MPI_Scatterv( data, scounts, displs, MPI_CHAR, recv_buf, recv_count, MPI_CHAR,
 			ROOT_ID, comm);
@@ -87,6 +104,8 @@ void thres_main(int myid, int p_tot, char *img_path) {
 	unsigned sum, tot_sum;
 
 	sum = sum_chunk( recv_buf, recv_count );
+
+	printf("Sum for id:%d, sum:%d\n", myid, sum);
 
 	MPI_Allreduce(&sum, &tot_sum, 1, MPI_UNSIGNED, MPI_SUM, comm);
 
@@ -96,7 +115,10 @@ void thres_main(int myid, int p_tot, char *img_path) {
 
 	MPI_Gatherv( recv_buf, recv_count, MPI_CHAR, data, scounts, displs, MPI_CHAR, ROOT_ID, comm);
 
+	free(recv_buf);
+
 	if( myid == ROOT_ID ){
+		printf("Totsum: %d\n", tot_sum);
 		write_ppm( "./out.ppm", x_size, y_size, (char *) data);
 
 		free(data);
@@ -114,6 +136,9 @@ int main( int argc, char **argv )
 		return -1;
 	}
 
+	double starttime, endtime;
+	starttime = MPI_Wtime();
+
 	int myid, p_tot;
 	MPI_Init( NULL, NULL );
 	MPI_Comm_rank( MPI_COMM_WORLD, &myid );
@@ -122,6 +147,8 @@ int main( int argc, char **argv )
 	thres_main( myid, p_tot, argv[1] );
 
 	MPI_Finalize();
+	endtime = MPI_Wtime();
+	printf("That took %f seconds\n",endtime-starttime);
 	return 0;
 }
 
