711a852c425f7811e43799e1de31e5fd72637622
Sebastian Parborg <darkdefende@gmail.com>
2016-04-29 12:32:56 +0200

    Updated tests scripts with openmp thread count

--- lab3/run2.sh
+++ lab3/run2.sh
@@ -5,9 +5,9 @@ for n in {1..10}; do
 	echo "Iteration: $n ,Cores: $1" >> result2.txt
 	echo "Iteration: $n ,Cores: $1" >> result3.txt
 	echo "Iteration: $n ,Cores: $1" >> result4.txt
-	./test1.out $1 >> result1.txt
-	./test2.out $1 >> result2.txt
-	./test3.out $1 >> result3.txt
-	./test4.out $1 >> result4.txt
+	OMP_NUM_THREADS=$1 ./test1.out >> result1.txt
+	OMP_NUM_THREADS=$1 ./test2.out >> result2.txt
+	OMP_NUM_THREADS=$1 ./test3.out >> result3.txt
+	OMP_NUM_THREADS=$1 ./test4.out >> result4.txt
 done
 
--- lab3/test1.sh
+++ lab3/test1.sh
@@ -2,6 +2,6 @@
 
 for n in {1..10}; do
 	echo "Iteration: $n" >> $3
-	$1 $2 >> $3
+	OMP_NUM_THREADS=$2 $1 >> $3
 done
 
